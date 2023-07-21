fb462f17be41541dadc40b45b3b3abe4265cd4ad
Mostafa Wael <109765494+Mostafa-Wael-Elsahity@users.noreply.github.com>
2023-07-21 22:54:59 +0300

    Time: 206 ms (81.20%), Space: 75.2 MB (94.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int maxProfit(vector<int>& p) {
         int n = p.size(),k=2;
-        vector<vector<int>> dp(n + 1, vector<int>(2 * k + 1));
+        vector<int> dp(2 * k + 1), temp(2 * k + 1);
         
         for(int i=n-1; i>=0; i--) {
             for(int t=0; t<2 * k; t++) {
                 if(t % 2 == 0) {
-                    dp[i][t] = max(-p[i] + dp[i + 1][t + 1], 0 + dp[i + 1][t]);
+                    temp[t] = max(-p[i] + dp[t + 1], 0 + dp[t]);
                 }
-                else dp[i][t] = max(p[i] + dp[i + 1][t + 1], 0 + dp[i + 1][t]);
+                else temp[t] = max(p[i] + dp[t + 1], 0 + dp[t]);
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
