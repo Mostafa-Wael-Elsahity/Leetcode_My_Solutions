d08a9e6e3dff835764edea13525e6e21ae33029c
Mostafa Wael <109765494+Mostafa-Wael-Elsahity@users.noreply.github.com>
2023-07-28 11:10:23 +0300

    Time: 4 ms (63.90%), Space: 7.4 MB (75.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -5,13 +5,11 @@ public:
         memset(memo,-1,sizeof memo);
         for(auto &num:nums) sum+=num;
         function<int(int,int)> solve=[&](int i,int j)->int{
-            if(i>j) return 0;
+            if(i==j) return nums[i];
             int &res=memo[i][j];
             if(~res) return res;
-            if((i+j)%2!=n%2) res=max(nums[i]+solve(i+1,j),nums[j]+solve(i,j-1));
-            else res=min(solve(i+1,j),solve(i,j-1));
-            return res;
+            return res=max(nums[i]-solve(i+1,j),nums[j]-solve(i,j-1));
         };
-        return solve(0,n-1)*2>=sum;
+        return solve(0,n-1)>=0;
     }
 };
