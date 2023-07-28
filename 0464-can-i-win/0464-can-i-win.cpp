6357af2f33741cbb7cbe4c6c2df28f14059cdc63
Mostafa Wael <109765494+Mostafa-Wael-Elsahity@users.noreply.github.com>
2023-07-28 11:40:14 +0300

    Time: 141 ms (63.15%), Space: 49.9 MB (48.19%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.cpp
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -5,17 +5,19 @@ public:
     }
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
         if(getSum(maxChoosableInteger)<desiredTotal) return false;
-        vector<int>dp(1<<maxChoosableInteger,-1);
-        function<bool(int,int)> game=[&](int mask,int score)->bool{
-            if(dp[mask]!=-1) return dp[mask];
-            for(int i=0;i<maxChoosableInteger;i++)
-            {
-                int cmask = 1<<i;
-                if( (mask&cmask) == 0 )
-                        if(score+i+1 >= desiredTotal or game((mask|cmask),score+i+1)==false ) return dp[mask]=1;
+        vector<int>memo(1<<maxChoosableInteger,-1);
+        function<bool(int,int)> solve=[&](int mask,int score)->bool{
+            if(~memo[mask]) return memo[mask];
+            for(int i=0;i<maxChoosableInteger;i++){
+                int cmask=1<<i;
+                if(!(mask&cmask)){
+                    if(score+i+1>=desiredTotal || !solve(cmask|mask,score+i+1)){
+                        return memo[mask]=1;
+                    }
+                }
             }
-            return dp[mask]=0;
+            return memo[mask]=0;
         };
-        return game(0,0);
+        return solve(0,0);
     }
 };
