0d48fae94122a747ab0e65a52eee11443f9145be
Mostafa Wael <109765494+Mostafa-Wael-Elsahity@users.noreply.github.com>
2023-07-29 15:53:26 +0300

    Time: 17 ms (45.63%), Space: 6.4 MB (87.45%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     double soupServings(int n) {
         int m = ceil(n / 25.0);
-        unordered_map<int, unordered_map<int, double>> memo;
+        double memo[200][200]={};
         function<double(int, int)> calculateDP = [&](int i, int j) -> double {
             if (i <= 0 && j <= 0)  return 0.5;
             if (i <= 0)  return 1;
             if (j <= 0)  return 0;
-            if (memo[i].count(j))  return memo[i][j];
+            if (memo[i][j])  return memo[i][j];
             return memo[i][j] = (calculateDP(i - 4, j) + calculateDP(i - 3, j - 1) +
-                               calculateDP(i - 2, j - 2) + calculateDP(i - 1, j - 3)) /
-                              4;
+                               calculateDP(i - 2, j - 2) + calculateDP(i - 1, j - 3))*0.25;
         };
         for (int k = 1; k <= m; k++) {
             if (calculateDP(k, k) > 1 - 1e-5) {
