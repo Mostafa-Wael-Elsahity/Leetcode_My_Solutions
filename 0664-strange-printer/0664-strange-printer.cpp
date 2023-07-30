a134d6ef4937a47fbebd325479dec0f2bf1771d0
Mostafa Wael <109765494+Mostafa-Wael-Elsahity@users.noreply.github.com>
2023-07-30 12:33:49 +0300

    Time: 24 ms (89.29%), Space: 18.5 MB (13.19%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int strangePrinter(string s) {
-        int n=s.size(),memo[n+5][n+5];
-        memset(memo,-1,sizeof memo);
+        int n=s.size();
+        string str; // to compression s
+        for(int i=0; i<n;){
+            char ch = s[i];
+            while(i<n && ch==s[i]) i++;
+            str.push_back(ch);
+        }
+        s=str;
+        vector<vector<int>>memo(101,vector<int>(101,-1));
         function<int(int,int)> dp=[&](int l,int r)->int{
+            if(l>r) return 0;
             int &res=memo[l][r];
             if(~res) return res;
-            res=n;
-            int pos=-1;
-            for(int i=l;i<r;i++){
-                if(s[i]!=s[r]&&pos==-1) pos=i;
-                if(~pos) res=min(res,1+dp(pos,i)+dp(i+1,r));
-            }
-            if(pos==-1) res=0;
+            res=1+dp(l+1,r);
+            for(int i=l+1;i<=r;i++)
+                if(s[i]==s[l])
+                    res=min(res,dp(i,r)+dp(l+1,i-1));
             return res;
         };
-        return dp(0,n-1)+1;
+        return dp(0,s.size()-1);
     }
 };
